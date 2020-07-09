64e74f4bc3045cd77f55a20dd7966e65543fe23d
ccw924263 <ccw924263059>
2020-07-09 16:59:09 +0800

    面试题 17.13. 恢复空格

---
+++ src/main/java/leetcode/done/ReSpaceLcci.java
@@ -0,0 +1,69 @@
+package leetcode.done;
+
+import java.util.Arrays;
+
+public class ReSpaceLcci {
+    class Solution {
+        public int respace(String[] dictionary, String sentence) {
+            int n = sentence.length();
+
+            Trie root = new Trie();
+            for (String word: dictionary) {
+                root.insert(word);
+            }
+
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+            for (int i = 1; i <= n; ++i) {
+                dp[i] = dp[i - 1] + 1;
+
+                Trie curPos = root;
+                for (int j = i; j >= 1; --j) {
+                    int t = sentence.charAt(j - 1) - 'a';
+                    if (curPos.next[t] == null) {
+                        break;
+                    } else if (curPos.next[t].isEnd) {
+                        dp[i] = Math.min(dp[i], dp[j - 1]);
+                    }
+                    if (dp[i] == 0) {
+                        break;
+                    }
+                    curPos = curPos.next[t];
+                }
+            }
+            return dp[n];
+        }
+
+        class Trie {
+            public Trie[] next;
+            public boolean isEnd;
+
+            public Trie() {
+                next = new Trie[26];
+                isEnd = false;
+            }
+
+            public void insert(String s) {
+                Trie curPos = this;
+
+                for (int i = s.length() - 1; i >= 0; --i) {
+                    int t = s.charAt(i) - 'a';
+                    if (curPos.next[t] == null) {
+                        curPos.next[t] = new Trie();
+                    }
+                    curPos = curPos.next[t];
+                }
+                curPos.isEnd = true;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"jxnonurhhuanyuqahjy","phrxu","hjunypnyhajaaqhxduu"};
+        String b = "qahurhoharrdjxnonurhhuanyuqahjyppnha";
+        int result = new ReSpaceLcci().new Solution().respace(a,b);
+        System.out.println("输出：" + result);
+    }
+
+}
